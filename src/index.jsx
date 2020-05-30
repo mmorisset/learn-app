6227ef963cdb641aa1ecd317f4566161484f80ec
Maxime Morisset <maximemorisset@macbook-pro-de-maxime.home>
2020-05-30 18:17:38 +0200

    Setup project

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import 'whatwg-fetch';
+
+import './stylesheets/index.scss';
+
+import App from './components/App';
+
+import theme from './theme';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+var webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackSlimPlugin = require('html-webpack-slim-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DotenvFlow = require('dotenv-flow-webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpeg|png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      "src": path.resolve(__dirname, 'src/'),
+      "components": path.resolve(__dirname, 'src/components/'),
+      "containers": path.resolve(__dirname, 'src/constainers/'),
+      "services": path.resolve(__dirname, 'src/services/')
+    }
+  },
+  plugins: [
+    new DotenvFlow(),
+    new webpack.ProvidePlugin({
+      'Holder': 'holderjs',
+      'holder': 'holderjs',
+      'window.Holder': 'holderjs'
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Learn App',
+      myPageHeader: 'Learn App',
+      template: './src/index.html'
+    }),
+  ],
+};
